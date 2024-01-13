21e6aa42642b95b3e021ae38a2dcd142f16f6acb
opxzy22 <aopeyem84@gmail.com>
2024-01-13 00:08:34 +0000

    done

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,12 @@
-export default function getListStudents() {
+/**
+ * getListStudents: function that return an array of objects
+ * return array of object with key values
+ */
+function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
+export default getListStudents;
