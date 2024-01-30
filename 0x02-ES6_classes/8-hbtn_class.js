cc371db1c407211025e0c3a89ac2b3dae41cc9f1
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2024-01-30 20:06:34 +0100

    casting

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,15 +1,29 @@
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
-    this.location = location;
+    this._location = location;
   }
 
-  get size() {
-    return this._size;
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._location = value;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('size must be a Number');
+    }
+    this._location = value;
   }
 
   get location() {
     return this._location;
+  }
+
+  get size() {
+    return this._size;
   }
 
   valueOf() {
